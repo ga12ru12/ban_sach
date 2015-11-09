687dbe44b555934764aac161ea714e1928b7b7d0
ga21ru21@gmail.com <quang123>
2015-11-09 19:47:35 +0700

    init

--- routes/customer.js
+++ routes/customer.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
 
 router.get('/', function(req, res){
     res.render('customer/index');
 });
+
+router.post('/complete', function(req, res){
+    console.log(req.body);
+    if(req.body){
+        async.forEach(req.body, function(){
+
+        }, function(){
+
+        });
+    }
+})
 
 module.exports = router;
